c71b9cbe94b35c87bc171f96363b11e0474d3fc4
Eric <ericanox@gmail.com>
2026-08-08 14:08:53 +0100

    cors issues fixing

--- src/pages/404page.jsx
+++ src/pages/404page.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-6xl font-bold text-gray-800">404</h1>
+        <h1 className="text-6xl font-bold text-gray-800"> Error 404</h1>
         <p className="text-2xl text-gray-600 mt-4">Page Not Found</p>
         <Link
           to="/"
